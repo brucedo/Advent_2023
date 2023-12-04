f7e39ffaf501154ab1f244d04de939f0a7ca1cb4
Doug Bruce <queazan@gmail.com>
2023-12-03 19:33:18 -0600

    - Doooone

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,25 +1,108 @@
 defmodule Day3 do
+require Logger
+
+  def run() do
+    lines = Common.open(File.cwd, "day3.txt") |> Common.read_file_pipe() |> Common.close()
+
+    parts = parse_lines(lines, 0)
+
+    {numbers, symbols} = split_parts(parts)
+
+    adjacency_map = capture_symbols(symbols)
+
+    adjacent_sum = Enum.filter(numbers, &Day3.is_adjacent?(&1, adjacency_map)) |> Enum.map(&elem(&1, 1)) |> Enum.sum()
+
+    IO.puts("The sum of all adjacent numbers is #{adjacent_sum}")
+
+    gears_only = symbols |> Enum.filter(fn element -> elem(element, 1) == "*" end)
+    gear_map = capture_symbols(gears_only)
+
+    ratio_map = Enum.filter(numbers, &Day3.is_adjacent?(&1, gear_map))
+      |> Enum.map(fn (number) -> {Day3.gear_point(number, gear_map), elem(number, 1)} end)
+      |> List.foldl(%{}, fn e, acc ->
+          elem(Map.get_and_update(acc, elem(e, 0), fn v -> {v, if v == nil do [elem(e, 1)] else [elem(e, 1)] ++ v end} end), 1) end)
+
+    gear_ratios = Enum.filter(Map.values(ratio_map), fn ratios -> length(ratios) == 2 end)
+      |> Enum.map(&Enum.product/1)
+
+    IO.puts("The sum of all gear ratios, if I have not royally fucked up, is #{Enum.sum(gear_ratios)}")
+
+
+  end
+
+  def gear_point(number, adjacency_map) do
+    left_point = elem(number, 2)
+    right_point = %Point{x: left_point.x + length(Integer.digits(elem(number, 1))) - 1, y: left_point.y}
+
+    left_adj = Map.get(adjacency_map, left_point)
+    right_adj = Map.get(adjacency_map, right_point)
+
+    adjacent = if (left_adj == nil) do
+      right_adj
+    else
+      left_adj
+    end
+
+    if (length(adjacent) > 1) do
+      raise "There are, in fact, numbers adjacent to more than one gear.  yeesh."
+    end
+
+    Enum.map(adjacent, &elem(&1, 0)) |> List.first()
+
+  end
+
+  def is_adjacent?(number, adjacency_map) do
+    left_point = elem(number, 2)
+    right_point = %Point{x: left_point.x + length(Integer.digits(elem(number, 1))) - 1, y: left_point.y}
+
+    Map.has_key?(adjacency_map, left_point) || Map.has_key?(adjacency_map, right_point)
+  end
+
+  def split_parts(parts_list) do
+    flattened = List.flatten(parts_list)
+    part_numbers = Enum.filter(flattened, fn part -> elem(part, 0) == :Number end)
+    symbols = Enum.filter(flattened, fn part -> elem(part, 0) == :Symbol end)
+
+    {part_numbers, symbols}
+  end
+
+  def capture_symbols([]) do
+    %{}
+  end
+
+  def capture_symbols([element | remaining]) do
+    fill_adjacency(capture_symbols(remaining), elem(element, 2), elem(element, 1))
+  end
+
+  def parse_lines([], _) do
+    []
+  end
+
+  def parse_lines([line | lines], index) do
+    [(parse_line(line) |> Enum.map(&pointify &1, index))] ++ parse_lines(lines, index + 1)
+  end
 
   @spec fill_adjacency(map(), Point.p(), String.t())  :: map()
   def fill_adjacency(adjacency_map, point, symbol) do
 
-    Map.get_and_update(adjacency_map, %Point{x: point.x - 1, y: point.y - 1}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+    reference = {point, symbol}
+    Map.get_and_update(adjacency_map, %Point{x: point.x - 1, y: point.y - 1}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x, y: point.y - 1}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x, y: point.y - 1}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x + 1, y: point.y - 1}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x + 1, y: point.y - 1}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x - 1, y: point.y}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x - 1, y: point.y}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(point, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(point, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x + 1, y: point.y}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x + 1, y: point.y}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x - 1, y: point.y + 1}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x - 1, y: point.y + 1}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x, y: point.y + 1}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x, y: point.y + 1}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1) |>
-      Map.get_and_update(%Point{x: point.x + 1, y: point.y + 1}, fn val -> {val, if val == nil do [symbol] else [symbol] ++ val end} end ) |>
+      Map.get_and_update(%Point{x: point.x + 1, y: point.y + 1}, fn val -> {val, if val == nil do [reference] else [reference] ++ val end} end ) |>
       elem(1)
   end
 
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -1,4 +1,5 @@
 defmodule Day3Test do
+  require Logger
   use ExUnit.Case
   doctest Day3
 
@@ -34,8 +35,8 @@ defmodule Day3Test do
 
     adjacent = assert Map.get(origin_adj, %Point{x: 1, y: 1})
 
-    assert Enum.any?(adjacent, fn s -> s == "P" end)
-    assert Enum.any?(adjacent, fn s -> s == "Q" end)
+    assert Enum.any?(adjacent, fn s -> elem(s, 1) == "P" end)
+    assert Enum.any?(adjacent, fn s -> elem(s, 1) == "Q" end)
     assert length(adjacent) == 2
   end
 
@@ -51,6 +52,97 @@ defmodule Day3Test do
     assert Day3.pointify(input, 6) == {:Number, 617, %Point{x: 0, y: 6}}
   end
 
+  test "Given an appropriat sequence of mixed numbers & symbols & .s, parse_lines will convert them into a list of lists of parsed elements" do
+    lines = ["467..114..", "...*......", "..35..633.", "......#...", "617*......", ".....+.58.",
+      "..592.....", "......755.", "...$.*....", ".664.598.."]
 
+    parsed_lines = Day3.parse_lines(lines, 0)
+
+    assert parsed_lines == [
+      [{:Number, 467, %Point{x: 0, y: 0}}, {:Number, 114, %Point{x: 5, y: 0}}],
+      [{:Symbol, "*", %Point{x: 3, y: 1}}],
+      [{:Number, 35, %Point{x: 2, y: 2}}, {:Number, 633, %Point{x: 6, y: 2}}],
+      [{:Symbol, "#", %Point{x: 6, y: 3}}],
+      [{:Number, 617, %Point{x: 0, y: 4}}, {:Symbol, "*", %Point{x: 3, y: 4}}],
+      [{:Symbol, "+", %Point{x: 5, y: 5}}, {:Number, 58, %Point{x: 7, y: 5}}],
+      [{:Number, 592, %Point{x: 2, y: 6}}],
+      [{:Number, 755, %Point{x: 6, y: 7}}],
+      [{:Symbol, "$", %Point{x: 3, y: 8}}, {:Symbol, "*", %Point{x: 5, y: 8}}],
+      [{:Number, 664, %Point{x: 1, y: 9}}, {:Number, 598, %Point{x: 5, y: 9}}]
+    ]
+  end
+
+  test "Given a mixed flattened list of parsed elements, split_parts will return {:Number_types_only, :Symbol_types_only}" do
+    test_set = [
+      {:Number, 467, %Point{x: 0, y: 0}}, {:Number, 114, %Point{x: 5, y: 0}},
+      {:Symbol, "*", %Point{x: 3, y: 1}}, {:Number, 35, %Point{x: 2, y: 2}},
+      {:Number, 633, %Point{x: 6, y: 2}}, {:Symbol, "#", %Point{x: 6, y: 3}},
+      {:Number, 617, %Point{x: 0, y: 4}}, {:Symbol, "*", %Point{x: 3, y: 4}},
+      {:Symbol, "+", %Point{x: 5, y: 5}}, {:Number, 58, %Point{x: 7, y: 5}},
+      {:Number, 592, %Point{x: 2, y: 6}}, {:Number, 755, %Point{x: 6, y: 7}},
+      {:Symbol, "$", %Point{x: 3, y: 8}}, {:Symbol, "*", %Point{x: 5, y: 8}},
+      {:Number, 664, %Point{x: 1, y: 9}}, {:Number, 598, %Point{x: 5, y: 9}}
+    ]
+
+    assert Day3.split_parts(test_set) == {
+      [
+        {:Number, 467, %Point{x: 0, y: 0}}, {:Number, 114, %Point{x: 5, y: 0}},
+        {:Number, 35, %Point{x: 2, y: 2}}, {:Number, 633, %Point{x: 6, y: 2}},
+        {:Number, 617, %Point{x: 0, y: 4}}, {:Number, 58, %Point{x: 7, y: 5}},
+        {:Number, 592, %Point{x: 2, y: 6}}, {:Number, 755, %Point{x: 6, y: 7}},
+        {:Number, 664, %Point{x: 1, y: 9}}, {:Number, 598, %Point{x: 5, y: 9}}
+      ],
+      [
+        {:Symbol, "*", %Point{x: 3, y: 1}}, {:Symbol, "#", %Point{x: 6, y: 3}},
+        {:Symbol, "*", %Point{x: 3, y: 4}}, {:Symbol, "+", %Point{x: 5, y: 5}},
+        {:Symbol, "$", %Point{x: 3, y: 8}}, {:Symbol, "*", %Point{x: 5, y: 8}}
+      ]
+    }
+  end
+
+  test "Just go ahead and run the sample from the challenge already" do
+    lines = ["467..114..", "...*......", "..35..633.", "......#...", "617*......", ".....+.58.",
+      "..592.....", "......755.", "...$.*....", ".664.598.."]
+
+    parsed_elements = Day3.parse_lines(lines, 0)
+    {numbers, symbols} = Day3.split_parts(parsed_elements)
+
+    adjacency_map = Day3.capture_symbols(symbols)
+
+    adjacent_sum = Enum.filter(numbers, &Day3.is_adjacent?(&1, adjacency_map)) |> Enum.map(&elem(&1, 1)) |> Enum.sum()
+
+    assert adjacent_sum == 4361
+
+  end
+
+  test "Test part 2 now too." do
+    lines = ["467..114..", "...*......", "..35..633.", "......#...", "617*......", ".....+.58.",
+      "..592.....", "......755.", "...$.*....", ".664.598.."]
+
+      parsed_elements = Day3.parse_lines(lines, 0)
+      {numbers, symbols} = Day3.split_parts(parsed_elements)
+
+      gears_only = symbols |> Enum.filter(fn element -> elem(element, 1) == "*" end)
+      gear_map = Day3.capture_symbols(gears_only)
+
+      temp = Enum.filter(numbers, &Day3.is_adjacent?(&1, gear_map))
+      Logger.debug("#{inspect temp}")
+      temp = Enum.map(temp, fn (number) -> {Day3.gear_point(number, gear_map), elem(number, 1)} end)
+      Logger.debug("#{inspect temp}")
+      temp = List.foldl(temp, %{}, fn e, acc ->
+        elem(Map.get_and_update(acc, elem(e, 0), fn v -> {v, if v == nil do [elem(e, 1)] else [elem(e, 1)] ++ v end} end), 1) end)
+
+      Logger.debug("#{inspect temp}")
+
+      temp = Enum.filter(Map.values(temp), fn ratios -> length(ratios) == 2 end)
+
+      Logger.debug("#{inspect temp}")
+
+      temp = Enum.map(temp, fn pair -> Enum.product(pair) end)
+      Logger.debug("#{inspect temp}")
+
+      assert Enum.sum(temp) == 467835
+
+  end
 
 end
