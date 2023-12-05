cecf736fd52eba6fb5b70d3a4d9e117a16b368a5
Doug Bruce <queazan@gmail.com>
2023-12-05 17:11:00 -0600

    - just starting...

---
+++ lib/day5.ex
@@ -0,0 +1,18 @@
+defmodule Day5 do
+
+  @spec consume_range(String.t()) :: map()
+  def consume_range(range_line) do
+
+  end
+
+  @spec consume_ranges(list(String.t())) :: map()
+  def consume_ranges(ranges) do
+
+  end
+
+  @spec break_almanac(list(String.t())) :: map()
+  def break_almanac(lines) do
+
+  end
+
+end
