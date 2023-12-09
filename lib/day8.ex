7aea522b3ce92405e3c593f93e193a5993f32bd3
Doug Bruce <queazan@gmail.com>
2023-12-08 21:49:04 -0600

    - well this is garbage but it works.  I did start on a GCD-based implementation but didn't get it done before the crappy version finished.

--- lib/day8.ex
+++ lib/day8.ex
@@ -1,4 +1,5 @@
 defmodule Day8 do
+require Logger
 
   def run do
     lines = Common.open(File.cwd, "day8.txt") |> Common.read_file_pipe() |> Common.close()
@@ -6,13 +7,69 @@ defmodule Day8 do
     path = List.first(lines) |> String.graphemes()
     map = List.delete_at(lines, 0) |> List.delete_at(0) |> make_map()
 
-    {state, steps} = solve_maze(map, path)
+    steps = solve_maze(map, path)
 
-    case state do
-      :ok -> IO.puts("Map solved in #{Integer.to_string(steps)} steps.")
-      _ -> IO.puts("Could not solve map: #{inspect(steps)}")
+    IO.puts("The list of solution times is #{inspect steps}")
+
+    growth_vector = for _ <- steps, do: 0
+    grow_by = List.to_tuple(steps)
+    growth_vector = List.replace_at(growth_vector, 0, elem(grow_by, 0))
+
+
+    lcm = shitty_lcm(growth_vector, grow_by)
+
+    IO.puts("LCM is #{inspect lcm} ???? ")
+
+    # case state do
+    #   :ok -> IO.puts("Map solved in #{Integer.to_string(steps)} steps.")
+    #   _ -> IO.puts("Could not solve map: #{inspect(steps)}")
+    # end
+
+  end
+
+  @spec find_gcd(%{integer() => integer()}, integer()) :: integer()
+  def find_gcd(gcd_map, count) do
+    Map.to_list(gcd_map)
+      |> Enum.filter(fn {_, value} -> value == count end)
+      |> Enum.reduce({0, 0}, fn {key, value}, {prev_key, prev_value} -> if {key > prev_key} do {key, value} else {prev_key, prev_value} end end)
+      |> elem(0)
+  end
+
+  @spec find_divisors(integer()) :: list(integer())
+  def find_divisors(number) do
+    Enum.filter(1..number, fn divisor -> Integer.mod(number, divisor) == 0 end)
+  end
+
+  @spec group_divisors(%{integer() => integer()}, list(integer())) :: %{integer() => integer()}
+  def group_divisors(divisor_map, divisors) do
+    List.foldl(divisors, divisor_map, fn divisor, map -> Map.get_and_update(map, divisor, fn count -> {count, if count == nil do 1 else count + 1 end} end) end)
+  end
+
+  @spec shitty_lcm(list(integer()), {}) :: integer()
+  def shitty_lcm(current_vector, add_by) do
+    case is_lcm?(current_vector) do
+      true -> List.first(current_vector)
+      false -> grow_smallest(current_vector, add_by) |> shitty_lcm(add_by)
     end
+  end
 
+  @spec grow_smallest(list(integer()), tuple()) :: list(integer())
+  def grow_smallest(current_vector, add_by) do
+    index = smallest_index(current_vector)
+    value_to_grow = Enum.fetch(current_vector, index) |> elem(1)
+    List.replace_at(current_vector, index, value_to_grow + elem(add_by, index))
+  end
+
+  @spec smallest_index(list(integer())) :: integer()
+  def smallest_index(vector) do
+    smallest = Enum.sort(vector) |> List.first()
+    Enum.find_index(vector, fn elem -> elem == smallest end)
+  end
+
+  @spec is_lcm?(list(integer())) :: boolean()
+  def is_lcm?(vector) do
+    start = List.first(vector)
+    Enum.all?(vector, fn value -> value == start end)
   end
 
   @spec make_map(list()) :: %{atom() => {atom(), atom()}}
@@ -37,12 +94,12 @@ defmodule Day8 do
   end
 
   def solve_path(map, ["R" | rest], last_nodes) do
-    updated_nodes = for node <- last_nodes, do: (Map.get(map, node) |> elem(1))
-
-    case done?(updated_nodes) do
+    # updated_nodes = for node <- last_nodes, do: (Map.get(map, node) |> elem(1))
+    updated_node = Map.get(map, last_nodes) |> elem(1)
+    case done?(updated_node) do
       true -> {:ok, 1}
       false ->
-        case solve_path(map, rest, updated_nodes) do
+        case solve_path(map, rest, updated_node) do
           {:ok, steps} -> {:ok, steps + 1}
           x -> x
         end
@@ -54,11 +111,12 @@ defmodule Day8 do
   end
 
   def solve_path(map, ["L" | rest], last_nodes) do
-    updated_nodes = for node <- last_nodes, do: (Map.get(map, node) |> elem(0))
-    case done?(updated_nodes) do
+    # updated_nodes = for node <- last_nodes, do: (Map.get(map, node) |> elem(0))
+    updated_node = Map.get(map, last_nodes) |> elem(0)
+    case done?(updated_node) do
       true -> {:ok, 1}
       false ->
-        case solve_path(map, rest, updated_nodes) do
+        case solve_path(map, rest, updated_node) do
           {:ok, steps} -> {:ok, steps + 1}
           x -> x
         end
@@ -70,30 +128,41 @@ defmodule Day8 do
     # end
   end
 
-  @spec done?(list(atom())) :: boolean()
+  @spec done?(atom()) :: boolean()
   def done?(nodes) do
-    List.foldl(nodes, true, fn elem, acc -> (Atom.to_string(elem) |> String.ends_with?("Z")) && acc end)
+    Atom.to_string(nodes) |> String.ends_with?("Z")
+    # List.foldl(nodes, true, fn elem, acc -> (Atom.to_string(elem) |> String.ends_with?("Z")) && acc end)
   end
 
-  @spec solve_maze(%{atom() => {atom(), atom()}}, list(String.t())) :: {atom(), atom()} | {atom(), integer()}
+  @spec solve_maze(%{atom() => {atom(), atom()}}, list(String.t())) :: list(integer()) #{atom(), atom()} | {list(integer())}
   def solve_maze(map, path) do
-    solve_maze_with_state(map, path, :AAA, MapSet.new(), 0)
+    Logger.debug("The starting map is #{inspect map}")
+    start_set = find_start_set(map)
+    Logger.debug("The start set is #{inspect start_set}")
+    for starter <- start_set, do: (solve_maze_with_state(map, path, starter, MapSet.new()) |> elem(1))
   end
 
-  @spec solve_maze_with_state(%{atom() => {atom(), atom()}}, list(String.t()), atom(), MapSet.t(atom()), integer()) :: {atom(), atom()} | {atom(), integer()}
-  defp solve_maze_with_state(map, path, next_node, visited, tries) do
-    case MapSet.member?(visited, next_node) do
-      true -> {:error, :unsolvable}
-      false ->
+  @spec solve_maze_with_state(%{atom() => {atom(), atom()}}, list(String.t()), [atom()], MapSet.t(atom())) :: {atom(), atom()} | {atom(), integer()}
+  defp solve_maze_with_state(map, path, next_node, visited) do
+    # case MapSet.member?(visited, next_node) do
+    #   true -> {:error, :unsolvable}
+    #   false ->
         case solve_path(map, path, next_node) do
           {:error, last_node} ->
-            case solve_maze_with_state(map, path, last_node, MapSet.put(visited, next_node), tries + 1) do
+            case solve_maze_with_state(map, path, last_node, MapSet.put(visited, next_node)) do
               {:ok, step_count} -> {:ok, step_count + length(path)}
-              err -> err
+              # err -> err
             end
           ok -> ok
       end
-    end
+    # end
+  end
+
+  @spec find_start_set(%{atom() => {atom(), atom()}}) :: list(atom())
+  def find_start_set(map) do
+    Logger.debug("#{inspect Map.keys(map)}")
+    Logger.debug("#{Map.keys(map) |> Enum.map(fn node -> Atom.to_string(node) end)}")
+    Map.keys(map) |> Enum.filter(fn node -> Atom.to_string(node) |> String.ends_with?("A") end)
   end
 
 end
--- test/day8_test.exs
+++ test/day8_test.exs
@@ -17,12 +17,12 @@ defmodule Day8Test do
     assert Day8.solve_path(map, path, [:AAA]) == {:error, [:EEE]}
   end
 
-  test "when given a pair that is a map of nodes and a sequence of choices that lead to an infinite loop, solve_maze will return {:err, :unsolvable}" do
-    map = %{AAA: {:BBB, :CCC}, BBB: {:DDD, :EEE}, CCC: {:ZZZ, :GGG}, DDD: {:DDD, :DDD}, EEE: {:EEE, :EEE}, GGG: {:GGG, :GGG}, ZZZ: {:ZZZ, :ZZZ}}
-    path = ["L", "R"]
+  # test "when given a pair that is a map of nodes and a sequence of choices that lead to an infinite loop, solve_maze will return {:err, :unsolvable}" do
+  #   map = %{AAA: {:BBB, :CCC}, BBB: {:DDD, :EEE}, CCC: {:ZZZ, :GGG}, DDD: {:DDD, :DDD}, EEE: {:EEE, :EEE}, GGG: {:GGG, :GGG}, ZZZ: {:ZZZ, :ZZZ}}
+  #   path = ["L", "R"]
 
-    assert Day8.solve_maze(map, path) == {:error, :unsolvable}
-  end
+  #   assert Day8.solve_maze(map, path) == {:error, :unsolvable}
+  # end
 
   test "when given a pair that is a map of nodes and a sequence of choices that will solve the map on the first pass, solve_maze will return {:ok, step_count}" do
     map = %{AAA: {:BBB, :CCC}, BBB: {:DDD, :EEE}, CCC: {:ZZZ, :GGG}, DDD: {:DDD, :DDD}, EEE: {:EEE, :EEE}, GGG: {:GGG, :GGG}, ZZZ: {:ZZZ, :ZZZ}}
@@ -62,6 +62,21 @@ defmodule Day8Test do
     nodes = [:ONEONEZ, :TWOTWO, :THREETHREE, :ZZZ]
 
     assert !Day8.done?(nodes)
+  end
+
+  test "Given a map filled with node relationships, find_start_set will find the list of all atoms that start with A" do
+    map = %{:ZZA => {:BAB, :CAC}, :ZAZ => {:CCA, :CCC}, :BAB => {:ZZA, :ZAZ}, :CAC => {:CCA, :CCC}, :AAA => {:CCA, :CCC}, :CCA => {:AAA, :CCC}}
+
+    assert Day8.find_start_set(map) |> Enum.sort() == [:AAA, :CCA, :ZZA]
+  end
+
+  test "Try this with a part 2 equivalent dataset now..." do
+    map = %{:OOA => {:OOB, :XXX}, :OOB => {:XXX, :OOZ}, :OOZ => {:OOB, :XXX},
+            :TTA => {:TTB, :XXX}, :TTB => {:TTC, :TTC}, :TTC => {:TTZ, :TTZ},
+            :TTZ => {:TTB, :TTB}, :XXX => {:XXX, :XXX}}
+    path = ["L", "R"]
+
+    assert Day8.solve_maze(map, path) == {:ok, 6}
   end
 
 end
