302b9cdab3cfc945c90d165a8590b3c62b9a1491
Doug Bruce <queazan@gmail.com>
2023-12-10 16:38:26 -0600

    - Part 1 done.  Part 2 looks to be a small bag of dicks.

--- lib/day10.ex
+++ lib/day10.ex
@@ -1,8 +1,149 @@
 defmodule Day10 do
+require Logger
 
-  @spec tile_to_points(Point.p(), String.t()) :: {Point.p(), Point.p()}
-  def tile_to_points(coordinate, symbol) do
+  @type pointmap() :: %{Point.p() => {Point.p(), Point.p()}}
+
+  def run() do
+    lines = Common.open(File.cwd, "day10.txt") |> Common.read_file_pipe() |> Common.close()
+
+    start_point = find_start(lines)
+
+    map = load_map(lines)
+
+    case trace_route(map, start_point) do
+      {:ok, steps} -> IO.puts("A complete circuit is #{Integer.to_string(steps)}, so the furthest point should be #{Integer.to_string(div(steps, 2))}")
+    end
+
 
   end
+
+  @spec find_walk(pointmap(), Point.p()) :: list(Point.p())
+  def find_walk(map, start) do
+
+  end
+
+  @spec sort_ascending(list(Point.p())) :: list(Point.p())
+  def sort_ascending(raster_line) do
+
+  end
+
+  @spec rasterize(list(Point.p()))  :: %{Point.p() => list(Point.p())}
+  def rasterize(unrasterized) do
+
+  end
+
+  @spec trace_route(pointmap(), Point.p()) :: {:ok, integer()} | {:error, String.t()}
+  def trace_route(map, start) do
+    IO.puts("Map: #{inspect map}")
+
+    path_starts = [%Point{x: start.x + 1, y: start.y}, %Point{x: start.x, y: start.y + 1}, %Point{x: start.x - 1, y: start.y}, %Point{x: start.x, y: start.y - 1}]
+
+    paths = Enum.map(path_starts, fn start_point -> walk_path(map, start, start, start_point) end)
+    path_tuple = paths
+      |> Enum.filter( fn {success, _} -> success == :ok end)
+      |> List.first(nil)
+
+
+    case path_tuple do
+      nil -> {:error, "No path could be found from starting point (#{start.x}, #{start.y})"}
+      win -> win
+    end
+  end
+
+  @spec find_start(list(String.t())) :: Point.p()
+  def find_start(lines) do
+    find_start(lines, 0)
+  end
+
+  defp find_start([], _) do
+    raise("There was no start point in this map.")
+  end
+  defp find_start([curr_line | lines], row) do
+    case String.contains?(curr_line, "S") do
+      true -> %Point{x: String.graphemes(curr_line) |> Enum.find_index(fn elem -> elem == "S" end), y: row }
+      false -> find_start(lines, row + 1)
+    end
+  end
+
+  @spec walk_path(pointmap(), Point.p(),  Point.p(), Point.p()) :: {:ok, integer()} | {:error, String.t()}
+  defp walk_path(_, start, _, next_point)
+  when start == next_point
+  do
+    IO.puts("Have found our way to the end of this path.")
+    {:ok, 1}
+  end
+  defp walk_path(map, start, current_point, next_point) do
+    # advance
+    connections = Map.get(map, next_point)
+    IO.puts("Checking point path #{inspect current_point} to  #{inspect next_point} and it is connected to #{inspect connections}")
+    case next_exit(current_point, connections) do
+      {:error, msg} -> {:error, msg}
+      {:ok, exit_point} ->
+        case walk_path(map, start, next_point, exit_point) do
+          {:ok, path_length} -> {:ok, path_length + 1}
+          otherwise -> otherwise
+        end
+    end
+
+  end
+
+  @spec next_exit(Point.p(), {Point.p(), Point.p()}) :: {:ok, Point.p()} | {:error, String.t()}
+  defp next_exit(point, destination)
+  when elem(destination, 0) == point
+  do
+    {:ok, elem(destination, 1)}
+  end
+  defp next_exit(point, destination)
+  when elem(destination, 1) == point
+  do
+    {:ok, elem(destination, 0)}
+  end
+  defp next_exit(point, pipe) do
+    {:error, "The point #{inspect point} does not actually connect to either end of the pipe #{inspect pipe}."}
+  end
+
+  @spec load_map(list(String.t())) :: %{Point.p => {Point.p(), Point.p()}}
+  def load_map(lines) do
+    load_map(lines, 0)
+  end
+
+  defp load_map([], _) do
+    %{}
+  end
+  defp load_map([line | rest], row) do
+    Map.merge(load_line(line, row), load_map(rest, row + 1))
+  end
+
+  @spec load_line(String.t(), integer()) :: %{Point.p() => {Point.p(), Point.p()}}
+  def load_line(line, row) do
+
+    String.graphemes(line)
+      |> Enum.zip(0..(String.length(line) - 1))
+      |> Enum.filter(fn {symbol, _} -> Regex.match?(~r/[|\-LJ77F]/, symbol) end)
+      |> Enum.map(fn {symbol, index} -> {symbol, %Point{x: index, y: row}} end)
+      |> List.foldl(%{}, fn {symbol, point}, acc -> Map.put(acc, point, tile_to_points(point, symbol)) end)
+  end
+
+  @spec tile_to_points(Point.p(), String.t()) :: {Point.p(), Point.p()}
+  def tile_to_points(coordinate, "|") do
+    {%Point{x: coordinate.x, y: coordinate.y - 1}, %Point{x: coordinate.x, y: coordinate.y + 1}}
+  end
+  def tile_to_points(coordinate, "-") do
+    {%Point{x: coordinate.x - 1, y: coordinate.y}, %Point{x: coordinate.x + 1, y: coordinate.y}}
+  end
+  def tile_to_points(coordinate, "L") do
+    {%Point{x: coordinate.x, y: coordinate.y - 1}, %Point{x: coordinate.x + 1, y: coordinate.y}}
+  end
+  def tile_to_points(coordinate, "J") do
+    {%Point{x: coordinate.x, y: coordinate.y - 1}, %Point{x: coordinate.x - 1, y: coordinate.y}}
+  end
+  def tile_to_points(coordinate, "7") do
+    {%Point{x: coordinate.x - 1, y: coordinate.y}, %Point{x: coordinate.x, y: coordinate.y + 1}}
+  end
+  def tile_to_points(coordinate, "F") do
+    {%Point{x: coordinate.x + 1, y: coordinate.y}, %Point{x: coordinate.x, y: coordinate.y + 1}}
+  end
+
+
 
 end
--- test/day10_test.exs
+++ test/day10_test.exs
@@ -1,4 +1,6 @@
 defmodule Day10Test do
+  require Logger
+  alias ExUnit.Assertions
   use ExUnit.Case
   doctest(Day10)
 
@@ -33,9 +35,137 @@ defmodule Day10Test do
   end
 
   test "Given a F symbol and its coordinates (x, y), tile_to_points will return two points (x+1, y) and (x, y+1)" do
-    pipe_endpionts = Day10.tile_to_points(%Point(x: 5, y: 5), "F")
+    pipe_endpoints = Day10.tile_to_points(%Point{x: 5, y: 5}, "F")
 
-    assert pipe_endpoints == {%Point{x: 6 y: 5}, %Point{x: 5, y: 6}}
+    assert pipe_endpoints == {%Point{x: 6, y: 5}, %Point{x: 5, y: 6}}
+  end
+
+  test "Given some input line with one | and a row index, load_line will return a map with one element whose key is that symbol's point and whose values are points above and below the symbol" do
+    line = "....|...."
+    row = 5
+
+    map = Day10.load_line(line, row)
+
+    assert Map.has_key?(map, %Point{x: 4, y: 5})
+    assert map_size(map) == 1
+    assert Map.get(map, %Point{x: 4, y: 5}) == {%Point{x: 4, y: 4}, %Point{x: 4, y: 6}}
+  end
+
+  test "Given som einput line with one each of the symbols, load_line will return a map with one element per symbol" do
+    line = "|-LJ7F"
+
+    map = Day10.load_line(line, 6)
+
+    assert Map.get(map, %Point{x: 0, y: 6}) == {%Point{x: 0, y: 5}, %Point{x: 0, y: 7}}
+    assert Map.get(map, %Point{x: 1, y: 6}) == {%Point{x: 0, y: 6}, %Point{x: 2, y: 6}}
+    assert Map.get(map, %Point{x: 2, y: 6}) == {%Point{x: 2, y: 5}, %Point{x: 3, y: 6}}
+    assert Map.get(map, %Point{x: 3, y: 6}) == {%Point{x: 3, y: 5}, %Point{x: 2, y: 6}}
+    assert Map.get(map, %Point{x: 4, y: 6}) == {%Point{x: 3, y: 6}, %Point{x: 4, y: 7}}
+  end
+
+  test "Given some set of input lines, load_map will do the thing it's supposed to do, this isn't a real project, stop trying to explain everything all nice and shit" do
+    lines = ["......|", "-......"]
+
+    map = Day10.load_map(lines)
+
+    assert Map.has_key?(map, %Point{x: 6, y: 0})
+    assert Map.has_key?(map, %Point{x: 0, y: 1})
+  end
+
+  test "Given a point map and a starting point on a loop of pipe, trace_route will return {:ok, length} where length is the number of elements in the pipe including S" do
+    map = %{
+      # %Point{x: 1, y: 1} => {%Point{x: 2, y: 1}, %Point{x: 1, y: 2}},
+      %Point{x: 2, y: 1} => {%Point{x: 1, y: 1}, %Point{x: 3, y: 1}},
+      %Point{x: 3, y: 1} => {%Point{x: 2, y: 1}, %Point{x: 3, y: 2}},
+      %Point{x: 1, y: 2} => {%Point{x: 1, y: 1}, %Point{x: 1, y: 3}},
+      %Point{x: 3, y: 2} => {%Point{x: 3, y: 1}, %Point{x: 3, y: 3}},
+      %Point{x: 1, y: 3} => {%Point{x: 1, y: 2}, %Point{x: 2, y: 3}},
+      %Point{x: 2, y: 3} => {%Point{x: 1, y: 3}, %Point{x: 3, y: 3}},
+      %Point{x: 3, y: 3} => {%Point{x: 2, y: 3}, %Point{x: 3, y: 2}}
+    }
+
+    start = %Point{x: 1, y: 1}
+
+    route = Day10.trace_route(map, start)
+
+    assert route == {:ok, 8}
+
+  end
+
+  test "pull it all together for test 1" do
+    lines = [
+      "-L|F7",
+      "7S-7|",
+      "L|7||",
+      "-L-J|",
+      "L|-JF"
+    ]
+
+    start_point = Day10.find_start(lines)
+
+    assert start_point == %Point{x: 1, y: 1}
+
+    map = Day10.load_map(lines)
+
+
+    assert Day10.trace_route(map, start_point) == {:ok, 8}
+  end
+
+  test "Given a map and a starting point, find_walk will return a list of elements that are involved in the walk from start to start" do
+    map = %{
+      # %Point{x: 1, y: 1} => {%Point{x: 2, y: 1}, %Point{x: 1, y: 2}},
+      %Point{x: 2, y: 1} => {%Point{x: 1, y: 1}, %Point{x: 3, y: 1}},
+      %Point{x: 3, y: 1} => {%Point{x: 2, y: 1}, %Point{x: 3, y: 2}},
+      %Point{x: 1, y: 2} => {%Point{x: 1, y: 1}, %Point{x: 1, y: 3}},
+      %Point{x: 3, y: 2} => {%Point{x: 3, y: 1}, %Point{x: 3, y: 3}},
+      %Point{x: 1, y: 3} => {%Point{x: 1, y: 2}, %Point{x: 2, y: 3}},
+      %Point{x: 2, y: 3} => {%Point{x: 1, y: 3}, %Point{x: 3, y: 3}},
+      %Point{x: 3, y: 3} => {%Point{x: 2, y: 3}, %Point{x: 3, y: 2}}
+    }
+
+    walk = Day10.find_walk(map, %Point{x: 1, y: 1})
+
+    assert walk == [
+      %Point{x: 1, y: 1}, %Point{x: 2, y: 1}, %Point{x: 3, y: 1}, %Point{x: 1, y: 2},
+      %Point{x: 3, y: 2}, %Point{x: 1, y: 3}, %Point{x: 2, y: 3}, %Point{x: 3, y: 3}
+    ]
+
+  end
+
+  test "Given a map with unconnected pipes, find_walk will return only the list of elements that are involved in the walk" do
+    lines = [
+      "-L|F7",
+      "7S-7|",
+      "L|7||",
+      "-L-J|",
+      "L|-JF"
+    ]
+
+    start_point = Day10.find_start(lines)
+
+    map = Day10.load_map(lines)
+
+    walk = Day10.find_walk(map, start_point)
+
+    assert walk == [
+      %Point{x: 1, y: 1}, %Point{x: 2, y: 1}, %Point{x: 3, y: 1}, %Point{x: 1, y: 2},
+      %Point{x: 3, y: 2}, %Point{x: 1, y: 3}, %Point{x: 2, y: 3}, %Point{x: 3, y: 3}
+    ]
+  end
+
+  test "Given a list of Points, sort_ascending will return the list ordered by ascending x coordinate" do
+    points = [%Point{x: 2, y: 1}, %Point{x: 3, y: 1}, %Point{x: 1, y: 1}]
+
+    sorted = Day10.sort_ascending(points)
+
+    assert points == [%Point{x: 1, y: 1}, %Point{x: 2, y: 1}, %Point{x: 3, y: 1}]
+  end
+
+  test "Given a list of Points, rasterize will return a Map of points keyed by their y-coordinate and where each raster lines Points are ordered by ascending X value" do
+    points = [
+      %Point{x: 1, y: 1}, %Point{x: 2, y: 1}, %Point{x: 3, y: 1}, %Point{x: 1, y: 2},
+      %Point{x: 3, y: 2}, %Point{x: 1, y: 3}, %Point{x: 2, y: 3}, %Point{x: 3, y: 3}
+    ]
   end
 
 end
